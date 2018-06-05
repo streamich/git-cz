1dcf01a3723f91438f241be2ceda793c4d412102
Va Da <streamich@gmail.com>
2018-06-05 17:54:38 +0100

    test: 💍 check prompter method is exported for Commitizen

--- test/cz.test.js
+++ test/cz.test.js
@@ -1,3 +1,9 @@
+const cz = require('../dist/cz');
+
 describe('commitizen', () => {
-  it('works');
+  it('exports prompter function', () => {
+    if (typeof cz.prompter !== 'function') {
+      throw new TypeError('Expected to export "prompter" function for Commitizen.');
+    }
+  });
 });
