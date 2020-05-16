8cb282657b01b0ee5d7090c890c9d7d739c0a65b
streamich <streamich@gmail.com>
2020-05-16 16:57:35 +0200

    test: 💍 remove snapshot test

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,5 +1,5 @@
 const pkg = require('../package.json');
-const {runCLI, keys} = require('./testUtils');
+const {runCLI} = require('./testUtils');
 
 test('git-cz --help', async () => {
   const {getResult} = runCLI(['--help']);
@@ -19,26 +19,6 @@ test('git-cz --version', async () => {
 
 test('git-cz --non-interactive', async () => {
   const {getResult} = runCLI(['--non-interactive', '--dry-run']);
-
-  const result = await getResult();
-
-  expect(result).toMatchSnapshot();
-});
-
-test('git-cz', async () => {
-  const {getResult, write} = runCLI(['--dry-run']);
-
-  await write([
-    keys.down,
-    keys.enter,
-    'adding an awesome feature',
-    keys.enter,
-    'the feature description',
-    keys.enter,
-    'Some Breaking Changes',
-    keys.enter,
-    '#1'
-  ]);
 
   const result = await getResult();
 
